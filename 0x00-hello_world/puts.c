6d937016d0f0d1e8439531001a09c3ae34a98cb0
Anibuali Emeka <lazameks@gmail.com>
2023-03-17 19:31:39 +0000

    print a exact string

---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * put - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char Mystr = "programming is ike building a multilingual puzzle\n";
+	puts(Mystr);
+	
+	return 0;
+}
