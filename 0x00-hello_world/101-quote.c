99f0ee5f0fe4cb820b441297fd2c385265916e10
Anibuali Emeka <lazameks@gmail.com>
2023-03-18 20:21:25 +0000

    printing with printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
