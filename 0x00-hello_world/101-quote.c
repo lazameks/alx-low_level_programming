816f9b101e31fba3b628698ccc5de01ee015a797
Anibuali Emeka <lazameks@gmail.com>
2023-03-18 20:14:39 +0000

    printing with printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Prints without using printf or put
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
