4aa99135ae9bb0853db7d3b6177ea60186778a25
Anibuali Emeka <lazameks@gmail.com>
2023-03-19 13:24:40 +0000

    prints lowercase and uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,12 +9,13 @@
  */
 int main(void)
 {
-	int ch_1, ch_2;
+	int ch_1,ch_2;
 	
 	for (ch_1 = 'a'; ch_1 <= 'z'; ch_1++)
-                putchar(ch_1);
+       	        putchar(ch_1);
+
 	for (ch_2 = 'A'; ch_2 <= 'Z'; ch_2++)
 		putchar(ch_2);
         putchar('\n');
-        return (0);
+	return (0);
 }
