f74a2890ccf49dec31fc34c79a46c144da616242
Anibuali Emeka <lazameks@gmail.com>
2023-03-20 18:10:24 +0000

    prints all possible combinations of two two-digit numbers.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,43 +10,27 @@
  */
 int main(void)
 {
-	int a, b, c, d, i, j;
+	int num1, num2;
 
-	a = '0';
-	for (a = '0'; a <= '9'; a++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		b = '0';
-		for (b = '0'; b <= '8'; b++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			i = '0';
-			if (a > '0')
-				i = a;
-			else
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 98 && num2 == 99)
 				continue;
-			j = '0';
-			if (b > '0')
-				j = (b + 1);
-			else
-				continue;
-			for (c = i; c <= '9'; c++)
-			{
-				for (d = j; d <= '9'; d++)
-				{
-					putchar(a);
-					putchar(b);
-					putchar(' ');
-					putchar(c);
-					putchar(d);
-					if (a == '9' && b == '8' && c == '9' && d == '9')
-					{
-						putchar('\n');
-						continue;
-					}
-					putchar(',');
-					putchar(' ');
-				}
-			}
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
+
+	putchar('\n');
+
 	return (0);
 }
