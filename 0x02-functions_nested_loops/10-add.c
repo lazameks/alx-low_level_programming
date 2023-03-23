fdfd0ed831b9c63942315aa11761c727bd88e0be
Anibuali Emeka <lazameks@gmail.com>
2023-03-23 03:53:43 +0000

    adds teo numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Entry point that adds two numbers
+ * @i: stores first digit
+ * @k: stores the second digit
+ *
+ * Return: Always (i + k)
+ */
+int add(int i, int k)
+{
+	return (i + k);
+}
