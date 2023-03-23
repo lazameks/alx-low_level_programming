941bd6ab8c0888454d8b5be3775dfb9531a06c8d
Anibuali Emeka <lazameks@gmail.com>
2023-03-23 15:51:22 +0000

    comment out

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,6 +3,7 @@
 /**
 * main - causes an infinite loop
 * Return: 0
+* @\\o/\n: escaped character \
 */
 
 int main(void)
@@ -12,12 +13,13 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
+	/**
+	*while (i < 10) - infinite loop
+	*{
+	*	putchar(i);
+	*}
+	*/
 
-/**	while (i < 10)
-*	{
-*		putchar(i);
-*	}
-*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
