29048707ac52d883597ee5320ee9f43ab72a5c23
Anibuali Emeka <lazameks@gmail.com>
2023-03-23 03:31:18 +0000

    all the days of jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,23 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * jack_bauer - Entry point point for all the days of jack
+ * no return value
  */
+void jack_bauer(void)
+{
+	int a, b;
 
+	for (a = 0; a <= 23; a++)
+	{
+		for (b = 0; b <= 59; b++)
+		{
+			_putchar (a / 10 + '0');
+			_putchar (a % 10 + '0');
+			_putchar (':');
+			_putchar (b / 10 + '0');
+			_putchar (b % 10 + '0');
+			_putchar ('\n');
+		}
+	}
+}
