e5dc310ef02088efc5b919519f883600a82f05ef
Anibuali Emeka <lazameks@gmail.com>
2023-04-03 15:40:26 +0000

    memcpy

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strchr - returns pointer to the first occurrence of the character
+ * @s: the string character
+ * @c: the desired starting point of the string character
+ * Return: s or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
