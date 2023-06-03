5a87c9b896d470fbca7766de4cece65d1da35107
Anibuali Emeka <lazameks@gmail.com>
2023-06-03 08:29:25 +0000

    fizz-buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,34 @@ int main(void)
 	for (i = 1; i < 101; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
-			printf("FizzBuzz ");
+		{
+			if (i == 100)
+				printf("FizzBuzz");
+			else
+				printf("FizzBuzz ");
+		}
 		else if (i % 3 == 0)
-			printf("Fizz ");
+		{
+			if (i == 100)
+				printf("Fizz");
+			else
+				printf("Fizz ");
+		}
 		else if (i % 5 == 0)
-			printf("Buzz ");
-		else if (i == 100)
-			printf("%d", i);
+		{
+			if (i == 100)
+				printf("Buzz");
+			else
+				printf("Buzz ");
+
+		}
 		else
-			printf("%d ", i);
+		{
+			if (i == 100)
+				printf("%d", i);
+			else
+				printf("%d ", i);
+		}
 	}
 	printf("\n");
 	return (0);
