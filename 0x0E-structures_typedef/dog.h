f9933937cad35229316d7e468e5ff7e6ec82e093
Anibuali Emeka <lazameks@gmail.com>
2023-06-22 11:33:32 +0000

    defining a struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defining a dog struct
+ * @name: name char
+ * @age: age
+ * @owner: owner char
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
