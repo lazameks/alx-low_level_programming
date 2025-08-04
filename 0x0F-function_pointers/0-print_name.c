25c0c1562d2c3df80b89fa5a27f5b99b4bbeb3ee
Anibuali Emeka <lazameks@gmail.com>
2025-08-04 22:52:56 +0000

    Add printname function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+
+/**
+ * print_name - Function to print name in any format
+ * @name: name to be printed
+ * @f: funtion pointer
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
