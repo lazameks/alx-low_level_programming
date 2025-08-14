0fd46a93083db7cd1aaefde144ce0b316d9a747d
Anibuali Emeka <lazameks@gmail.com>
2025-08-14 13:03:55 +0000

    Add a program that prints the opcodes of its own main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry to print program opcode
+ *
+ * @argc: Number of argument must be two
+ * @argv: arg varaibles
+ *
+ * Return: (0)
+ */
+int main(int argc, char *argv[])
+{
+	unsigned char *opcodes;
+	int n, i;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	opcodes = (unsigned char *)main;
+	
+	for (i = 0; i < n; i++)
+	{
+		if (i > 0)
+			printf(" ");
+		printf("%02x", opcodes[i]);
+	}
+	printf("\n");
+	return 0;
+}
