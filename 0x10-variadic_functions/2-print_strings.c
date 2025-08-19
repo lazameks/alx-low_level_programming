9fb93a62d57355a4611d7c21c80641c379244419
Anibuali Emeka <lazameks@gmail.com>
2025-08-19 09:30:19 +0000

    Add a function that prints string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - prints strings, followed by a new line
+ * @seperator: string to print between strings
+ * @n: number of strings passed
+ * @...: variable strings
+ */
+void print_strings(const char *seperator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *str;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+
+		if (str == NULL)
+			printf("nil");
+		else
+			printf("%s", str);
+
+		if (seperator != NULL && i != (n - 1))
+			printf("%s", seperator);
+	}
+	printf("\n");
+}
