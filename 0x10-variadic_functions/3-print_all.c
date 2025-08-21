68b29b8eebf28e0e73d8dd28548766c46b3b1adb
Anibuali Emeka <lazameks@gmail.com>
2025-08-21 12:17:58 +0000

    A program that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,47 +1,49 @@
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 /**
- * print_all - prints anything
- * @format: list of types of arguments passed to the function
- * @...: variable argument
+ * print_all - Prints anything.
+ * @format: A list of types of arguments passed to the function.
+ * @...: variable argument to be printed.
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
 	unsigned int i = 0;
-	char *str;
 	char *sep = "";
+	char *str;
 
 	va_start(args, format);
-
 	while (format && format[i])
 	{
-		if (format[i] == 'c')
+		switch (format[i])
 		{
-			printf("%s%c", sep, va_arg(args, int));
+			case 'c':
+				printf("%s%c", sep, va_arg(args, int));
+				sep = ", ";
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(args, int));
+				sep = ", ";
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(args, double));
+				sep = ", ";
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				printf("%s", sep);
+				if (str == NULL)
+					printf("(nil)");
+				if (str != NULL)
+					printf("%s", str);
+				sep = ", ";
+				break;
+			default:
+				break;
 		}
-		if (format[i] == 'i')
-		{
-			printf("%s%d", sep, va_arg(args, int));
-		}
-		if (format[i] == 'f')
-		{
-			printf("%s%f", sep, va_arg(args, double));
-		}
-		if (format[i] == 's')
-		{
-			str = va_arg(args, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s%s", sep, str);
-		}
-
-		if (format[i] == 'c' || format[i] == 'i' ||
-				format[i] == 'f' || format[i] == 's')
-			sep = ", ";
 		i++;
 	}
-
 	printf("\n");
 	va_end(args);
 }
+
